3006d3f0b69356e211177495187469b92b1fc8ee
elonj <56976129+elonj@users.noreply.github.com>
2020-11-08 21:52:17 -0800

    Update YaFi.js

--- QuX/YaFi.js
+++ QuX/YaFi.js
@@ -2,7 +2,13 @@ let body = $response.body
 body=JSON.parse(body)
 adUnitsGet = body['adUnits']
 for(var i = 0; i < adUnitsGet.length; i++){
-    delete adUnitsGet[i]['adFrames']
+    //delete adUnitsGet[i]['nativeAdInfo']
+    adFramesGet = adUnitsGet[i]['adFrames']
+    for(var j = 0; j < adFramesGet.length; j++){
+      adSpaceLayoutGet = adFramesGet[j]['adSpaceLayout']
+      adSpaceLayoutGet['adWidth'] = 0
+      adSpaceLayoutGet['adHeight'] = 0
+    }
   }
 body=JSON.stringify(body)
 $done({body})
